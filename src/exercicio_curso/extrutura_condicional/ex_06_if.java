4dd6a1f14a5f69c4daadc3764be570f7eaa56468
Hugo <113729299+tokyodreams@users.noreply.github.com>
2023-04-12 14:16:51 -0300

    ex.06

--- src/exercicio_curso/extrutura_condicional/ex_06_if.java
+++ src/exercicio_curso/extrutura_condicional/ex_06_if.java
@@ -8,6 +8,19 @@ public class ex_06_if {
 
         double intervalo = scn.nextDouble();
         
-        
+        if (intervalo < 0.0 || intervalo > 100.0) {
+            System.out.println("Fora do intervalo");
+        } else if (intervalo <= 25.0) {
+            System.out.println("Intervalo [0,25]");
+        } else if (intervalo <= 50.0) {
+            System.out.println("Intervalo [25,50]");
+        } else if (intervalo <= 75.0) {
+            System.out.println("Intervalo [50,75]");
+        }
+        else {
+            System.out.println("Intervalo (75,100)");
+        }
+
+        scn.close();
     }
 }
