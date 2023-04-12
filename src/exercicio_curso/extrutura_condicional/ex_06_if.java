293df6ad51d361912d3aa3dd0a8adfaa03b10989
Hugo <113729299+tokyodreams@users.noreply.github.com>
2023-04-12 14:00:55 -0300

    comit

---
+++ src/exercicio_curso/extrutura_condicional/ex_06_if.java
@@ -0,0 +1,13 @@
+package exercicio_curso.extrutura_condicional;
+
+import java.util.Scanner;
+
+public class ex_06_if {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        double intervalo = scn.nextDouble();
+        
+        
+    }
+}
